4d273f1fc974426a25c0d5a784dcc4604f0b073e
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-01-07 11:56:01 +0530

    typeofOperator program added

---
+++ Javascript/typeofOperator.js
@@ -0,0 +1,23 @@
+
+let x = 10;
+console.log(typeof x);
+
+x = 'john';
+console.log(typeof x);
+
+x = {
+    name:'jack',
+}
+console.log(typeof x);
+
+let flag = true;
+console.log(typeof flag);
+let type = typeof flag;
+
+
+const arr = [1,2,3,4,5];
+console.log(typeof arr);
+
+
+const j = null;
+console.log(j);
