253933de6bd68c2ebb359d2b03c11dea2fd25387
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-01-12 14:32:16 +0530

    Added the dataTypes

---
+++ Javascript/constants.js
@@ -0,0 +1,10 @@
+let radius =5
+const pi = 3.14
+let area
+
+area = pi*radius*radius
+console.log(area)
+
+
+// constant 'const should be used when the value ir or should be unchanged.
+
---
+++ Javascript/variables.js
@@ -0,0 +1,5 @@
+let num = 2+2 // let is a keyword to create a variable.
+console.log(num)
+
+let userName = 'Santhosh'
+console.log(userName)
