7315142c9599f315dba3fd38b1613db92133ca49
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-02-10 12:34:01 +0530

    Added the few programs on varaibles and data types in 'Practice'

---
+++ Practice/Variable.js
@@ -0,0 +1,25 @@
+var bunty ='playing'
+console.log(bunty)
+
+var bunty = 'singing'
+console.log(bunty)
+
+console.log('--------------------')
+
+// using var keyword we can declare multiple times using same defined'var' name 
+// here 'bunty is declared two times. No error message is displayed.
+
+let school = 'primary'
+console.log(school)
+
+
+ school = 'pre-primary'
+ console.log(school);
+
+console.log('--------------------')
+
+const ground ='used for playing'
+console.log(ground)
+
+
+
---
+++ Practice/dataType.js
@@ -0,0 +1,25 @@
+
+// Primitive Datatypes;
+// 1. String
+// 2. Number
+// 3. Boolena
+// 4. Null
+// 5. Undefined
+
+// Complex Datatypes:
+
+// 1. Array
+// 2. Object
+
+
+
+
+
+
+
+
+
+
+var string = "This a String"
+console.log('string')
+
