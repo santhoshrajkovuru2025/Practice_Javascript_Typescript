f6c2828aac2d599e63a74c357d21761f65f27340
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-02-13 16:06:17 +0530

    Added the control flo statements

---
+++ Practice/controlFlow.js
@@ -0,0 +1,42 @@
+
+let M=10
+let N=11
+
+if(M == N){
+    console.log('M equlas to N')
+}else if(M>N){
+    console.log('M is greater than N ')
+}else{
+    console.log("M is less than N")
+}
+
+console.log("----------------------------")
+
+
+let a =5;
+let b = 6;
+
+if(a+b == 10){
+    console.log('Wrong Answer')
+}else if(a+b == 15){
+    console.log('incorrect calculation')
+}else{
+    console.log(' 11 is the right answer')
+}
+
+
+a =15;
+
+if(i=1,i<a,i++){
+    console.log(a);
+}
+
+//  using ternery operator
+
+x = 19
+y = 16
+
+let z = (x<y)? x:y
+console.log(z)
+let z1 = (x>y)? x:y
+console.log(z1)
