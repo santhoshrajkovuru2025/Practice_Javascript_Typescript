b423c7f6718735bc92f608e903de248bd6350f40
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-02-26 12:31:29 +0530

    Added Type Annotations in Typescript directory

---
+++ Typescript/Name.ts
@@ -0,0 +1,5 @@
+
+console.log('Hello.. Typescript')
+
+// .ts --> tsc (Typescript compiler) --> (converts to javascript) .js -->   Javascript RunTime Env: Browser, NodeJs, App.
+
---
+++ Typescript/TypeAnnotations.ts
@@ -0,0 +1,21 @@
+
+let fName: String = 'Sunthosh'
+let age:number =32; // it is known as type Annotation
+let Istrue = true // it is known as type intefernce
+console.log(Istrue)
+console.log('------------------')
+
+// Type Annotations: we are declaring the type of variable explicitly.
+
+let a:number =4;
+let b:any =5.2;
+
+console.log(a+b)
+console.log('------------------')
+
+// Type Inference: here no type is mentioned , when the code is compiled then the type is added in the compile time and executed.
+
+let v = 5.6;
+let w=  7.8;
+
+console.log(v+w)
