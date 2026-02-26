8dd88cd45b7a54c12a2e6e8338548e85e67c8b65
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-02-26 11:36:29 +0530

    Added the Promise code in Advanced_javascript

--- Advanced_Javascript/Promise.js
+++ Advanced_Javascript/Promise.js
@@ -7,4 +7,36 @@ new promise ((resolve,reject)=>{
 
 })  */
 
-    
+    const Register = () => {
+
+       return new Promise((resolve, reject) => {
+            setTimeout(()=>{
+                console.log('Please Register')
+                 resolve("Registration Successful")
+                //  reject("Registration failed") // used for reject block
+            }, 3000)
+        })
+    }
+
+     const Login = () => {
+        return new Promise((resolve, reject)=>{
+        setTimeout(()=> {
+            console.log("Please Login")
+             resolve() 
+            }, 1000) 
+        })
+     }
+
+     const Thankyou = () =>{
+        return new Promise((resolve, reject)=>{
+            setTimeout(() => {
+                console.log('Thank You !')
+                resolve()
+            },3000)
+
+        })
+     }
+
+     // Register().then((data)=>{console.log(data)}) : before adding the login function, i had added this.
+
+     Register().then(Login).then(Thankyou) //.catch((err)=>console.log(err))
