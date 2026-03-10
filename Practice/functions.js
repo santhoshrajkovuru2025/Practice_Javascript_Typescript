6b8d37de1120349506b84f994c4fbe12b9e38d51
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-03-10 12:44:02 +0530

    Added few lines of code in functions for the directory practice

--- Practice/functions.js
+++ Practice/functions.js
@@ -11,4 +11,29 @@ const multiple = (a,b) =>{
     return a*b
 };
 console.log(multiple(3,8))
+console.log("----------------------------------")
+
+// what is a function ? : It is a block of code
+// function with parameters and without parameters
+// function should contain function name
+// function key word should be ued to declare a function
+
+// function without parameters:
+
+function identity(){
+    console.log('Identity of the person is None');
+}
+identity();
+console.log("----------------------------------")
+
+// function with parameters:
+
+function identify(name,age){
+
+    console.log("The person name is:" +name, "and the age is: "+age)
+    console.log(`The name of the person is ${name} and the age is ${age}`)
+}
+identify('Swaroop',40)
+identify('Santhosh',25)
+
 
