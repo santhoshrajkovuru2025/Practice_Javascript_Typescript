de9077abb5a684a76a75e5482387901489522b5f
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-03-18 10:58:45 +0530

    Added Loops

---
+++ Typescript/whileLoops.js
@@ -0,0 +1,11 @@
+var i = 2;
+while (i <= 20) {
+    console.log(i);
+    i += 2;
+}
+console.log('-----------------------------');
+var j = 1;
+while (j <= 30) {
+    console.log(j);
+    j += 2;
+}
---
+++ Typescript/whileLoops.ts
@@ -0,0 +1,18 @@
+
+var i: number = 2
+
+while (i<=20){
+
+console.log(i)
+i+=2
+
+}
+console.log('-----------------------------')
+
+var j: number = 1
+
+while(j<=30){
+
+    console.log(j)
+    j +=2
+}
