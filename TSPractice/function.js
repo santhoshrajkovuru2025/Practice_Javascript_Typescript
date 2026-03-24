f63a2f0624443ba3ca849c320623bb4256e36154
santhoshrajkovuru2025 <srkovuru@gmail.com>
2026-03-24 09:18:46 +0530

    Added the functions in TS Practice

--- TSPractice/function.js
+++ TSPractice/function.js
@@ -6,3 +6,8 @@ function company(a, b) {
     return a + b;
 }
 console.log(company('softsol', 'india'));
+console.log('------------------------------------');
+var sum = function (x, y) {
+    return x + y;
+};
+console.log(sum(10, 20));
--- TSPractice/function.ts
+++ TSPractice/function.ts
@@ -1,13 +1,22 @@
 
-function add(a:number, b: number):number{
+function add1(a:number, b: number):number{
 
     return a+b;
 }
-console.log(add(10,20));
+console.log(add1(10,20));
 
 
-function company(a:string, b: string): string{
+function compani(a:string, b: string): string{
 
     return a+b;
 }
 console.log(company('softsol','india'));
+
+console.log('------------------------------------')
+
+let sum1 = function(x:number, y:number):number
+{
+    return x+y;
+
+}
+console.log(sum1(10,20));
